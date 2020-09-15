aaf1863da35d4fe8204c5780c8a3577d86bfe1ba
shrvn12120 <shrvn12120@gmail.com>
2020-09-15 22:07:39 +0500

    js file added

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle');
+    var banner = document.querySelector('.banner');
+    menuToggle.classList.toggle('active');
+    banner.classList.toggle('active');
+}
